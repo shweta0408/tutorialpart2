15054a16aca2730f69ca17c245f4eb35b0e00933
Shweta Mishra <shmis2004@gmail.com>
2022-08-27 13:54:33 +0530

    assignment

---
+++ script.js
@@ -0,0 +1,13 @@
+//strict mode
+'use strict'
+
+let hasDriversLicence = false
+const passTest = true
+
+if (passTest) hasDriversLicence = true
+if (hasDriversLicence) console.log("i can drive")
+
+//const interface = 'Audio'
+//session
+
+
