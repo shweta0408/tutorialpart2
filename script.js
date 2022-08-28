9e89f708e5689a5af9cf6d9319b7eef1b0eaae67
Shweta Mishra <shmis2004@gmail.com>
2022-08-28 19:25:43 +0530

    new function added

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ if (passTest) hasDriversLicence = true
 if (hasDriversLicence) console.log("i can drive")
 
 //const interface = 'Audio'
-//session
+//-----Functions------
 
+function logger(){
+    console.log('my name is shweta')
+}
+//calling,running,invoking
+logger()
 
+function fruitProcessor(apples, oranges) {
+    console.log(apples,oranges)
+    const juice = `Juice with ${apples} apples ${oranges} oranges`
+    return juice
+}
+
+const appleJuice = fruitProcessor(5, 0)
+console.log(appleJuice)
+
+const appleOrange = fruitProcessor(4,7)
+console.log(appleOrange)
+
+const appleOranges =  fruitProcessor(8,3)
+console.log(appleOranges)
