98e94ea091b51a8fc97540eba0fbe59728b5195c
Shweta Mishra <shmis2004@gmail.com>
2022-08-31 13:13:30 +0530

    arrow function

--- script.js
+++ script.js
@@ -1,51 +1,69 @@
 //strict mode
-'use strict'
+// 'use strict'
 
-let hasDriversLicence = false
-const passTest = true
+// let hasDriversLicence = false
+// const passTest = true
 
-if (passTest) hasDriversLicence = true
-if (hasDriversLicence) console.log("i can drive car")
+// if (passTest) hasDriversLicence = true
+// if (hasDriversLicence) console.log("i can drive car")
 
-//const interface = 'Audio'
-//-----Functions------
+// //const interface = 'Audio'
+// //-----Functions------
 
-function logger(){
-    console.log('my name is shweta')
-}
-//calling,running,invoking
-logger()
+// function logger(){
+//     console.log('my name is shweta')
+// }
+// //calling,running,invoking
+// logger()
 
-function fruitProcessor(apples, oranges) {
-    console.log(apples,oranges)
-    const juice = `Juice with ${apples} apples ${oranges} oranges`
-    return juice
-}
+// function fruitProcessor(apples, oranges) {
+//     console.log(apples,oranges)
+//     const juice = `Juice with ${apples} apples ${oranges} oranges`
+//     return juice
+// }
 
-const appleJuice = fruitProcessor(5, 0)
-console.log(appleJuice)
+// const appleJuice = fruitProcessor(5, 0)
+// console.log(appleJuice)
 
-const appleOrange = fruitProcessor(4,7)
-console.log(appleOrange)
+// const appleOrange = fruitProcessor(4,7)
+// console.log(appleOrange)
 
-const appleOranges =  fruitProcessor(5,3)
-console.log(appleOranges)
+// const appleOranges =  fruitProcessor(5,3)
+// console.log(appleOranges)
 
-const appleOrangesFruit =  fruitProcessor(5,2)
-console.log(appleOrangesFruit)
+// const appleOrangesFruit =  fruitProcessor(5,2)
+// console.log(appleOrangesFruit)
 
-//FUNCTION DECLARATION
-function calcAge(birthYear){
-    return 2022 - birthYear
-}
-const calcBirth1 = calcAge(2006)
+// //FUNCTION DECLARATION
+// function calcAge(birthYear){
+//     return 2022 - birthYear
+// }
+// const calcBirth1 = calcAge(2006)
 
-//FUNCTION EXPRESSION
-const calcBirth = function (birthYear){
-    return 2022-birthYear
-}
-const calcBirth4 = calcBirth(2004)
-console.log(calcBirth4, calcBirth1)
+// FUNCTION EXPRESSION
+// const calcBirth = function (birthYear){
+//     return 2022-birthYear
+// }
+// const calcBirth4 = calcBirth(2004)
+// console.log(calcBirth4, calcBirth1)
 
 //const calcBirth = calcAge(2004)
 //console.log(calcBirth)
+
+
+//ARROW FUNCTION
+ const calcAge = birthYear => 2022-birthYear
+ const myAge = calcAge(2004)
+ console.log(myAge)
+
+ const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2022-birthYear
+    const retirement = 65 - age
+    //return retirement
+    return `${firstName} retires in ${retirement} years `
+ }
+
+ console.log(yearsUntilRetirement(2004,'Shweta'))
+ console.log(yearsUntilRetirement(2002,'Pragya'))
+ 
+
