958a087c4fdbbb25873c172d347b553a64e8f608
Shweta Mishra <shmis2004@gmail.com>
2022-08-31 16:30:22 +0530

    array datastructures

--- script.js
+++ script.js
@@ -106,35 +106,77 @@ const yearsUntilRetirement = function (birthYear, firstName)
    // return `${firstName} retires in ${retirement} years `
  }
 
- console.log(yearsUntilRetirement(2004,'Shweta'))
- console.log(yearsUntilRetirement(1940,'testPeople'))
+//  console.log(yearsUntilRetirement(2004,'Shweta'))
+//  console.log(yearsUntilRetirement(1940,'testPeople'))
 
 
-//coding challenge
+// //coding challenge
 
 
-const calcAverage = (score1, score2, score3) => (score1+score2+score3)/3
-console.log(calcAverage(2,3,7))
+// const calcAverage = (score1, score2, score3) => (score1+score2+score3)/3
+// console.log(calcAverage(2,3,7))
 
-const scoreDolphins = calcAverage(44,23,71)
-const scoreKoalas = calcAverage(65,54,49)
+// const scoreDolphins = calcAverage(44,23,71)
+// const scoreKoalas = calcAverage(65,54,49)
 
-console.log(scoreDolphins,scoreKoalas)
+// console.log(scoreDolphins,scoreKoalas)
 
-const  checkWinner = function(avgDolphins, avgKoalas)
-{
-   if (avgDolphins >= 2* avgKoalas){
-        console.log(`winner is Dolphin with ${avgDolphins} vs ${avgKoalas}`)
-   } else if (avgKoalas >= 2* avgDolphins){
-      console.log(`winner is koala with ${avgKoalas} vs. ${avgDolphins}`)
-   } else {
-    console.log("no one is winner")
-   }
+// const  checkWinner = function(avgDolphins, avgKoalas)
+// {
+//    if (avgDolphins >= 2* avgKoalas){
+//         console.log(`winner is Dolphin with ${avgDolphins} vs ${avgKoalas}`)
+//    } else if (avgKoalas >= 2* avgDolphins){
+//       console.log(`winner is koala with ${avgKoalas} vs. ${avgDolphins}`)
+//    } else {
+//     console.log("no one is winner")
+//    }
 
+// }
+
+// checkWinner(scoreDolphins, scoreKoalas)
+// checkWinner(700,114)
+
+/*const calcAverage = (a,b,c) => (a,b,c)/3
+
+const scoreDolphins = calcAverage(33,44,56)
+const scoreKoala  = calcAverage(23,67,45)
+
+const checkWinner = function (avgDolphins,avgKoalas){
+
+    if (avgDolphins >= 2* avgKoalas){
+        console.log(`Dolphin wins with 
+        ${avgDolphins} vs ${avgKoalas}`)
+    } 
+    else if (avgKoalas >= 2* avgDolphins){
+        console.log( `Koala wins with score 
+        ${avgKoalas} vs ${avgDolphins}`)
+    } else {
+        console.log("no team wins")
+    }
+       
 }
 
-checkWinner(scoreDolphins, scoreKoalas)
-checkWinner(700,114)
+checkWinner(scoreDolphins,scoreKoala)
+
+*/
+
+const friends = ["Michel", "Steve", "Rob"]
+console.log(friends)
+
+const years = new Array(1991, 1992, 1993,1994)
+
+console.log(friends[0])
+console.log(friends.length)
+console.log(friends[friends.length -1])
+
+friends[2] = 'Vinnie'
+console.log(friends)
+
+const Shweta = ['Shweta', 'Mishra', 2022-2004]
+
+
+
+
 
 
 
