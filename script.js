f21ff23281152546382c96f6064a5b2519860a1c
Shweta Mishra <shmis2004@gmail.com>
2022-09-01 12:23:40 +0530

    coding challenge

--- script.js
+++ script.js
@@ -217,6 +217,17 @@ if(friends.includes('Rob')){
 
 //coding challenge 
 
+const calcTip = function(bill){
+    return bill >= 50 && bill <=300 
+    ? bill * 0.15
+     : bill * 0.2
+}
+
+const bills = [125, 555, 44]
+const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+const totals = [bills[0]+tip[0], bills[1]+tip[1], bills[2]+tip[2]]
+console.log(bills,tip, totals)
+
 
 
 
